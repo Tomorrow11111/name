87f4878a9f54ed7e0181e89ebb80fea9669fa601
pattarapong <phattaraphong.ph@ku.th>
2023-01-20 17:50:57 +0700

    first

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="show.php" method="post">
+    ชื่อ:<input type="text" name="name"><br>
+    อายุ:<input type="var"name="old">
+    เพศ:<input type="text"name="Sex"><br>
+    สถานะการแต่งงาน:<input type="text"name="S"><br>
+    <input type="submit" value="send"> 
+
+
+</body>
+</html>
